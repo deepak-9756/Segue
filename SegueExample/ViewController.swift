4c22f0a50fcaad4e871296f076c6cbc3d2d059cf
deepak-9756 <145149791+deepak-9756@users.noreply.github.com>
2025-12-01 22:16:34 +0530

    Initial Commit

---
+++ SegueExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SegueExample
+//
+//  Created by Merger on 01/12/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
