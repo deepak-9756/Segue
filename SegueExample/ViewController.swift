9265d6fd108f062e4b8a66bc1652b77da3c6143e
deepak-9756 <145149791+deepak-9756@users.noreply.github.com>
2025-12-18 22:19:05 +0530

    tested

--- SegueExample/ViewController.swift
+++ SegueExample/ViewController.swift
@@ -8,51 +8,13 @@
 import UIKit
 
 class ViewController: UIViewController  {
-     
-    @IBOutlet weak var tableView: UITableView!
-    
-    var users: [UserModel] = [UserModel(avtar: UIImage(systemName: "person")!, name: "Josh Hazalwood" , bio: "This is a first bio . This is a first bio . This is a first bio . This is a first bio . This is a first bio . This is a first bio ."), UserModel(avtar: UIImage(systemName: "person")!, name: "Ros Taylor" , bio: "This is a first bio . This is a first bio . This is a first bio . This is a first bio . This is a first bio . This is a first bio ."),UserModel(avtar: UIImage(systemName: "person")!, name: " Mitchell Starc" , bio: "This is a first bio . This is a first bio . This is a first bio .This is a first bio . This is a first bio . This is a first bio .")]
+   
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        tableView.delegate = self
-        tableView.dataSource = self
-        // setup with xib tableview cell
-        let nib = UINib(nibName: UserXibTableViewCell.identifier, bundle: nil)
-        tableView.register(nib, forCellReuseIdentifier: UserXibTableViewCell.identifier)
         
-        // setup with prograatic view cell
-        tableView.register(ProgramaticTableViewCell.self, forCellReuseIdentifier:  ProgramaticTableViewCell.identifier)
-        tableView.estimatedRowHeight = 85
-        tableView.rowHeight = UITableView.automaticDimension
     }
          
 }
    
     
-
-extension ViewController : UITableViewDataSource{
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-      return  users.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let user = users[indexPath.row]
-        let cell = tableView.dequeueReusableCell(withIdentifier: UserXibTableViewCell.identifier , for: indexPath) as! UserXibTableViewCell
-        cell.configure(user: user)
-         
-        return cell
-    }
-}
-    
-extension ViewController: UITableViewDelegate{
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-         
-    }
-    
-//    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-//        return 70
-//    }
-}
-
- 
